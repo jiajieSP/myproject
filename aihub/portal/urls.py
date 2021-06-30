9ac36b715e5fa7e5ef4d1c6ff5e7e8629c968dad
JJ25 <jjnoryip@gmail.com>
2021-06-30 20:44:44 +0800

    Login Feature Done
    Login Feature completed:
    1. General Color Scheme Integration
    2. Username + password Authentication
    3. Login/Logout Function
    
    To be reviewed:
    1. Element Alignments
    2. Account Creations and Password Reset (To be done under Admin Page)

--- aihub/portal/urls.py
+++ aihub/portal/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+from django.urls import path, include 
+from django.views.generic.base import TemplateView # new
+
+
 
 urlpatterns = [
     path('', views.home, name='index'),
-    path('add', views.add, name='add')
+    path('admin/', admin.site.urls),
+    path('acccounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home')
+    # path('add', views.add, name='add')
 ]
-
-
-# urlpatterns = ...
-
-urlpatterns += staticfiles_urlpatterns()
--- aihub/portal/views.py
+++ aihub/portal/views.py
@@ -3,18 +3,17 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
 # Create your views here.
 
 def home(request):
-    return render(request, 'login.html', {'name':'JiaJie'})
+    return render(request, 'index.html')
     # return HttpResponse("<h1>hello world my name is jia jie</h1>")
 
-@csrf_exempt
-def add(request):
+# @csrf_exempt
+# def add(request):
 
-    val1 = request.POST['username']
-    val2 = request.POST['password']
-    status = 'successful'
+#     val1 = request.POST['username']
+#     val2 = request.POST['password']
+#     status = 'successful'
 
-    return render(request, 'results.html', {'user' : val1, 'pass' : val2,'result': status})
+#     return render(request, 'results.html', {'user' : val1, 'pass' : val2,'result': status})
